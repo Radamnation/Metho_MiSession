7691ed234e49f6afe5db1f72fd58f37a881b718c
Radam <radamricher@gmail.com>
2023-04-01 10:24:16 -0400

    Added Persistent Scene for Managers

--- Assets/Scripts/FactoryDesignPattern/ProjectileFactory.cs
+++ Assets/Scripts/FactoryDesignPattern/ProjectileFactory.cs
@@ -16,20 +16,20 @@ public class ProjectileFactory : MonoBehaviour
         Destroy(gameObject);
     }
     
-    [SerializeField] private List<ProjectileData> projectileDataList;
-
-    public void SpawnRandomProjectile(Vector3 _position, Quaternion _rotation)
-    {
-        var projectileData = projectileDataList[Random.Range(0, projectileDataList.Count)];
-        
-        Projectile randomProjectile = PoolManager.Instance.GetProjectile();
-        randomProjectile.transform.SetPositionAndRotation(_position, _rotation);
-        randomProjectile.IsOrbital = projectileData.IsOrbital;
-        randomProjectile.Animator.runtimeAnimatorController = projectileData.animatorController;
-        randomProjectile.CircleCollider2D.radius = projectileData.colliderRadius;
-        
-        randomProjectile.Initialize();
-    }
+    // [SerializeField] private List<ProjectileData> projectileDataList;
+    //
+    // public void SpawnRandomProjectile(Vector3 _position, Quaternion _rotation)
+    // {
+    //     var projectileData = projectileDataList[Random.Range(0, projectileDataList.Count)];
+    //     
+    //     Projectile randomProjectile = PoolManager.Instance.GetProjectile();
+    //     randomProjectile.transform.SetPositionAndRotation(_position, _rotation);
+    //     randomProjectile.IsOrbital = projectileData.IsOrbital;
+    //     randomProjectile.Animator.runtimeAnimatorController = projectileData.animatorController;
+    //     randomProjectile.CircleCollider2D.radius = projectileData.colliderRadius;
+    //     
+    //     randomProjectile.Initialize();
+    // }
     
     public void SpawnProjectile(ProjectileData _projectileData, Vector3 _position, Quaternion _rotation)
     {
